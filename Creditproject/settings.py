f33187a05f3c08f502f115cab092714e7d097476
Shruti3893 <66069960+Shruti3893@users.noreply.github.com>
2020-11-08 21:11:09 +0530

    Update settings.py

--- Creditproject/settings.py
+++ Creditproject/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = '#cx3+&57%^s^=_dg&lv7_7vfz&%ph$%cplwn7kg05gg&^wr)ga'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ["revolving-credit.herokuapp.com"]
-ALLOWED_HOSTS = ["Shrutidb.pythonanywhere.com"]
+ALLOWED_HOSTS = ["revolving-credit.herokuapp.com"]
+#ALLOWED_HOSTS = ["Shrutidb.pythonanywhere.com"]
 
 
 # Application definition
